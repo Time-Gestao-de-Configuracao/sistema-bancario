23f39f6dbc60bd194e8003c6ed4ccb0d9ce09189
Yago Marques <yagobsmarques@gmail.com>
2021-07-14 22:05:53 -0300

    Adicionando esqueleto de funcionamento do sistema

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import views.Home;
+
+public class Main {
+
+	public Main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.print("//=== Bem-vindo ao SmartFarmacia! ===//\n\n");
+		Home.telaInicial(1);
+
+	}
+
+}
