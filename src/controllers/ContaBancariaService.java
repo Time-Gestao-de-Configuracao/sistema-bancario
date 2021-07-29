2f524bc687bd4129e21a7877ac7cad214f22b4f0
Yago Marques <yagobsmarques@gmail.com>
2021-07-28 22:22:42 -0300

    feature de adicionar funcionalidades para uma conta bônus

--- src/controllers/ContaBancariaService.java
+++ src/controllers/ContaBancariaService.java
@@ -1,65 +1,119 @@
 package controllers;
 
+import java.util.ArrayList;
+
 import dao.ContaBancariaDAO;
 import models.ContaBancaria;
+import models.ContaBancariaBonus;
 
-public class ContaBancariaService {
-	
+public class ContaBancariaService implements IContaBancariaService {
+
 	ContaBancariaDAO contaBancariaDAO;
-	
+
 	public ContaBancariaService() {
 		this.contaBancariaDAO = ContaBancariaDAO.getInstance();
 	}
-	
-	public int cadastrarConta(double saldo, int numeroIdentificador) {
+
+	@Override
+	public int cadastrarConta(double saldo, int numeroIdentificador, int tipo) {
 		if (this.contaBancariaDAO.procuraPeloId(numeroIdentificador) != null) {
 			return -1;
 		} else {
-			ContaBancaria novaConta = new ContaBancaria();
+			ContaBancaria novaConta;
+			switch (tipo) {
+			case 1:
+				novaConta = new ContaBancaria();
+				break;
+			case 2:
+				novaConta = new ContaBancariaBonus();
+				break;
+			default:
+				return -1;
+			}
 			novaConta.setNumeroIdentificador(numeroIdentificador);
 			novaConta.setSaldo(saldo);
 			this.contaBancariaDAO.inserir(novaConta);
+
 		}
 		return 0;
 	}
-	
-	public Double consultarSaldo (int numeroIdentificador) {
-				
-		return contaBancariaDAO.procuraPeloId(numeroIdentificador).getSaldo();
+
+	@Override
+	public ArrayList<Double> consultarSaldo(int numeroIdentificador) {
+		ContaBancaria contaBancaria = contaBancariaDAO.procuraPeloId(numeroIdentificador);
+		ArrayList<Double> result = new ArrayList<Double>();
+		result.add(contaBancaria.getSaldo());
+		if (contaBancaria instanceof ContaBancariaBonus) {
+			result.add((double) ((ContaBancariaBonus) contaBancaria).getBonus());
+		}
+		return result;
 	}
-	
-	public boolean creditarConta (int numeroIdentificador, Double saldo) {
-		
-		if (this.contaBancariaDAO.procuraPeloId(numeroIdentificador) == null) {
+
+	@Override
+	public boolean creditarConta(int numeroIdentificador, Double valor) {
+		ContaBancaria contaBancaria = contaBancariaDAO.procuraPeloId(numeroIdentificador);
+		IContaBancariaService serviceConta= getServiceStrategy(contaBancaria);
+		if (contaBancaria == null) {
 			return false;
 		} else {
-			Double valor = contaBancariaDAO.procuraPeloId(numeroIdentificador).getSaldo();
-			contaBancariaDAO.procuraPeloId(numeroIdentificador).setSaldo(saldo+valor);
+			if(!(serviceConta instanceof ContaBancariaService)) {
+				serviceConta.creditarConta(numeroIdentificador, valor);
+			} else {
+				Double saldo = contaBancaria.getSaldo();
+				contaBancaria.setSaldo(saldo + valor);
+			}
 		}
 		return true;
 	}
 
-	public boolean debitarConta (int numeroIdentificador, Double saldo) {
+	@Override
+	public boolean creditarContaTransferencia(int numeroIdentificador, Double valor) {
+
 		if (this.contaBancariaDAO.procuraPeloId(numeroIdentificador) == null) {
 			return false;
 		} else {
-			Double valor = contaBancariaDAO.procuraPeloId(numeroIdentificador).getSaldo();
-			contaBancariaDAO.procuraPeloId(numeroIdentificador).setSaldo(valor-saldo);
+			Double saldo = contaBancariaDAO.procuraPeloId(numeroIdentificador).getSaldo();
+			contaBancariaDAO.procuraPeloId(numeroIdentificador).setSaldo(saldo + valor);
 		}
 		return true;
 	}
 
+	@Override
+	public boolean debitarConta(int numeroIdentificador, Double valor) {
+		if (this.contaBancariaDAO.procuraPeloId(numeroIdentificador) == null) {
+			return false;
+		} else {
+			Double saldo = contaBancariaDAO.procuraPeloId(numeroIdentificador).getSaldo();
+			contaBancariaDAO.procuraPeloId(numeroIdentificador).setSaldo (saldo - valor);
+		}
+		return true;
+	}
+
+	@Override
 	public int transferirConta(int origem, int destino, Double saldo) {
-		if( debitarConta(origem, saldo) == true) {
-			boolean response = creditarConta(destino, saldo);
+		ContaBancaria contaBancariaOrigem = contaBancariaDAO.procuraPeloId(origem);
+		ContaBancaria contaBancariaDestino = contaBancariaDAO.procuraPeloId(destino);
+		IContaBancariaService serviceOrigem = getServiceStrategy(contaBancariaOrigem);
+		IContaBancariaService serviceDestino = getServiceStrategy(contaBancariaDestino);
+		if (serviceOrigem.debitarConta(origem, saldo) == true) {
+			boolean response = serviceDestino.creditarContaTransferencia(destino, saldo);
 			if (!response) {
-				creditarConta(origem, saldo);
+				serviceOrigem.creditarConta(origem, saldo);
 				return -1;
 			} else {
 				return 0;
 			}
-		} 
-		return  -2;
+		}
+		return -2;
+
+	}
 	
+	public IContaBancariaService getServiceStrategy(ContaBancaria contaBancaria) {
+		if(contaBancaria instanceof ContaBancariaBonus) {
+			return new ContaBancariaBonusService();
+		} else {
+			return new ContaBancariaService();
+		}
+		
 	}
 }
