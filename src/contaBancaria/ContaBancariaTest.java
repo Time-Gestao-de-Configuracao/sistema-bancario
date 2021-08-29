ca589d4d8de9a87dae88e5fa9fe3597d5f83b18a
marlusmarcos <marlusufrn@gmail.com>
2021-08-29 18:05:20 -0300

    teste unitario contaService

---
+++ src/contaBancaria/ContaBancariaTest.java
@@ -0,0 +1,41 @@
+package contaBancaria;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.Test;
+
+import controllers.ContaBancariaService;
+
+class ContaBancariaTest {
+	ContaBancariaService conta = new ContaBancariaService();
+	@Test
+	void testCadastrarConta() {
+		//ContaBancariaService conta = new ContaBancariaService();
+		int res = conta.cadastrarConta(10, 1, 1);
+		assertEquals(0, res);
+	}
+
+	@Test
+	void testConsultarSaldo() {
+		conta.cadastrarConta(10, 2, 1);
+		ArrayList<Double> saldo = conta.consultarSaldo(2);
+		boolean result = saldo.isEmpty();
+
+	}
+
+	@Test
+	void testCreditarConta() {
+		conta.cadastrarConta(10, 3, 1);
+		ArrayList<Double> saldo = conta.consultarSaldo(3);
+		assertEquals(10, saldo);
+	}
+
+	@Test
+	void testDebitarConta() {
+		fail("Not yet implemented");
+	}
+
+}
