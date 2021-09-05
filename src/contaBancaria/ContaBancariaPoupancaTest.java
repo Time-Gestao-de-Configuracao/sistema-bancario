5f435fe78d3ef998cb69bf33583b06372a02da61
Hagliberto <hagliberto@gmail.com>
2021-09-04 21:31:44 -0300

    Testes unitários para as operações da conta Poupança

--- src/contaBancaria/ContaBancariaPoupancaTest.java
+++ src/contaBancaria/ContaBancariaPoupancaTest.java
@@ -3,6 +3,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
@@ -30,6 +31,26 @@ class ContaBancariaPoupancaTest {
 		ArrayList<Double> saldo = conta.consultarSaldo(1);
 		assertEquals(10, saldo.get(0));
 		
+	}
+
+	@Test
+	void debitar(){
+		conta = new ContaBancariaService();
+		conta.cadastrarConta(50, 2, 2);
+		conta.debitarConta(2,20.00);
+
+		List<Double> saldoConta = conta.consultarSaldo(2);
+		assertEquals(30.00,saldoConta.get(0), 0.00);
+	}
+
+	@Test
+	void creditar(){
+		conta = new ContaBancariaService();
+		conta.cadastrarConta(0, 9, 2);
+		conta.creditarConta(9,20.00);
+
+		List<Double> saldoConta = conta.consultarSaldo(9);
+		assertEquals(20.00, saldoConta.get(0), 0.00);
 	}
 
 }
